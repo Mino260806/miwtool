0cb65885d669f1f8bce8769c74d449e4b364aa0f
Domenick90 <57414291+Domenick90@users.noreply.github.com>
2022-12-07 08:47:07 +0500

    Add files via upload

---
+++ bin/unpack.bat
@@ -0,0 +1,19 @@
+@echo off
+
+IF "%1"=="" GOTO :Continue
+
+set file=%~n1
+set mPath=%~dp1
+set mPathScript=%~dp0
+
+python main.py -d "%mPath%%file%"
+ren watchface.zip %file%_decoded.zip
+
+echo.
+echo Resources and JSON in "%mPath%%file%_decoded"
+echo.
+pause
+
+:Continue
+IF "%1"=="" echo No bin file given. Usage: %~nx0 path\to\file.bin
+
